0c0728e9a12a8894de9ac931c4c31d0e3c2796ee
DuyguA <duygu.altinok12@gmail.com>
2024-08-12 12:59:39 +0200

    articles final

--- bella_cleaner/filterer/configs/articles/quality_document.py
+++ bella_cleaner/filterer/configs/articles/quality_document.py
@@ -1,3 +1,5 @@
+import re
+
 copler = [
     "ġ",
     "Ģ",
@@ -20,39 +22,34 @@ copler = [
 ]
 
 def is_cop(text):
-  for cop in copler:
-    if cop in text:
-      print(cop)
-      return True
-  #if any([cop in text for cop in copler]):
-  #  return True
+  # Return if the doc has trash char sequences
+  if any([cop in text for cop in copler]):
+    return True
   if re.search(r"\w\$\w", text):
     return True
   return False
 
 
-
 def too_long_words(text):
+  # Return if the doc has words of length more than 50 chars, most probably many words without spaces.
   if re.search(r"\w{50}", text):
     return True
   return False
 
 
-
-
 def contains_mistakes(text):
+  # Return if the doc has too many single chars
   if re.search(r"\w \w \w \w \w", text):
     return True
   return False
 
 
 def filter(text):
+  # Main filter func
   text = text.strip()
   if not text:
     return True
   if contains_mistakes(text):
-    return True
-  if is_too_short(text):
     return True
   if is_cop(text):
     return True
