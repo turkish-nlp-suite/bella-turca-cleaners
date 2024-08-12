c94abe75e3b1942436db0a836bb372439dd808ff
DuyguA <duygu.altinok12@gmail.com>
2024-08-12 15:55:02 +0200

    added comments

--- bella_cleaner/filterer/engine.py
+++ bella_cleaner/filterer/engine.py
@@ -31,6 +31,7 @@ class Filterer:
       self.configure()
 
   def is_english(self, text):
+    # English sentence filtering helper
     try:
       isReliable, textBytesFound, details = cld2.detect(text)
       lang = details[0]
@@ -42,9 +43,11 @@ class Filterer:
 
 
   def filter_by_lang(self, text):
-    return self.is_english(text) # to filter or not to filter
+    # Filter out English sentences
+    return self.is_english(text) 
 
   def filter_sent_by_length(self, sentence):
+    # Filter out sentences shorter than the minimum word/char length given by the config. Returns True for too short sentences.
     if len(sentence) <= self.sent_char_lim:
       return True
     if self.sent_word_lim:
@@ -54,11 +57,13 @@ class Filterer:
     return False
 
   def filter_sentence(self, sentence):
+    # Filter out sentences with length, language, quality and topic criterion. Returns True for not-good sentences.
     return self.filter_sent_by_length(sentence) or self.filter_by_lang(sentence) or \
            self.sent_qual_filterer(sentence) or self.sent_topic_filterer(sentence)
 
 
   def filter_doc_by_length(self, doc, sentences):
+    # Filter out docs shorter than the minimum word/char length given by the config.Returns True for not short docs.
     if len(doc) <= self.doc_char_lim:
       return True
     if self.doc_word_lim:
@@ -71,11 +76,13 @@ class Filterer:
 
 
   def filter_doc(self, doc, sentences):
+    # Filter out docs with length, language, quality and topic criterion. Returns True for not-good docs.
     return self.filter_doc_by_length(doc, sentences) or self.filter_by_lang(doc) or \
            self.doc_qual_filterer(doc) or self.doc_topic_filterer(doc)
 
 
   def load_config(self):
+    # Load custom config file
     yaml_file = os.path.join(self.custom_dir, "custom.yaml")  
 
     with open(yaml_file, "r") as f:
@@ -83,11 +90,13 @@ class Filterer:
 
 
   def eval_config(self):
+    # Evaluate given custom config.
     yaml_keys = self.custom_config.keys()
     list_of_keys = ["topic", "length", "quality", "lang"]
     assert all(key in list_of_keys for key in yaml_keys), "Custom config keys should be topic, quality, length, lang."
 
   def configure(self):
+    # Parse the given custom config and set-up custom made functionality given by the config.
     self.topic_filt = self.custom_config.get("topic", [])
     self.handle_topic_filt()
 
@@ -100,12 +109,11 @@ class Filterer:
     self.len_lims = self.custom_config.get("length", {})
     self.handle_length_lims()
 
-    # Arrange keeping emojis
-
 
   def handle_topic_filt(self):
+    # Set up topic filtering methods.
     if self.topic_filt:
-      if "document" in sellf.topic_filt:
+      if "document" in self.topic_filt:
        module = importlib.import_module(self.custom_mod + ".topic_document", package="bella_cleaner")
        self.doc_topic_filterer = module.filter
       if "sentence" in self.topic_filt:
@@ -113,6 +121,7 @@ class Filterer:
        self.sent_topic_filterer = module.filter
 
   def handle_quality_filt(self):
+    # Set up quality filtering methods.
     if self.qual_filt:
       if "document" in self.qual_filt:
        module = importlib.import_module(self.custom_mod + ".quality_document", package="bella_cleaner")
@@ -123,6 +132,7 @@ class Filterer:
 
 
   def handle_lang(self):
+    # Set up language filtering methods.
     if self.langf:
       if "sentence" in self.langf:
           self.filter_sents_by_lang = True
@@ -131,7 +141,7 @@ class Filterer:
           self.filter_docs_by_lang = True
 
   def handle_length_lims(self):
-    #print(self.len_lims)
+    # Set up filtering by length methods.
     if not self.len_lims:
       return
     if self.len_lims:
@@ -157,6 +167,7 @@ class Filterer:
 
 
   def _filter(self, paragraph):
+    # Apply sentence filters to the all sentence in a paragraph and return the filtered paragraph.
     sentences = split_into_sentences(paragraph)
     sentences = [sentence for sentence in sentences if not self.filter_sentence(sentence)]
     parg = " ".join(sentences)
@@ -165,6 +176,7 @@ class Filterer:
 
 
   def filter(self, doc):
+    # Apply all the filters to the document and return the filtered document. If document should be filtered itself, returns None
     sentences = split_into_sentences(doc)
     fd = self.filter_doc(doc, sentences)
 
@@ -174,5 +186,7 @@ class Filterer:
     paragraphs = list(map(self._filter, paragraphs))
     paragraphs = [parg for parg in paragraphs if parg and not parg.isspace()] # filter possiby empty paragraphs
     doc = "\n".join(paragraphs)
+    if not doc:
+      return None
     return doc
 
